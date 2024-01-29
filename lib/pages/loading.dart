e243de5a5fe7d8b2b72648ac86f0df0cc420ed77
Aquib <aquibs732@gmail.com>
2024-01-29 20:10:48 +0530

    First Commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoactionState();
+}
+
+class _LoactionState extends State<Loading> {
+  void getData() async {
+    Response response =
+        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map data = jsonDecode(response.body);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    print('Inifjalfjalfjalfjalfjalft');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("ehllo"),
+    );
+  }
+}
